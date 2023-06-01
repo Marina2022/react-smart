3656e83622ff5d542c9580808149c9825a06e269
YAMISHKA02 <yashkur.misha@gmail.com>
2023-06-01 19:26:32 +0300

    chains update and some comments

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,20 +6,20 @@ import './App.scss';
 import {EthereumClient, w3mConnectors, w3mProvider} from '@web3modal/ethereum'
 import {Web3Modal} from '@web3modal/react'
 import {configureChains, createConfig, useAccount, useBalance, WagmiConfig} from 'wagmi'
-import {arbitrum, mainnet, polygon, polygonMumbai} from 'wagmi/chains'
+import {polygonMumbai} from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 import {useDispatch, useSelector} from "react-redux";
 import {selectWallet, setWallet} from "../../store/reducers/dataReducer";
 import {useEffect} from "react";
 import {ethers} from "ethers";
 
-const chains = [arbitrum, mainnet, polygon, polygonMumbai]
-const projectId = '678dd611607cf4d445084821cf371cb9'
+const chains = [polygonMumbai]
+const projectId = '678dd611607cf4d445084821cf371cb9' //вот это надо будет засунуть в .env файл
 
 const {publicClient} = configureChains(chains, [w3mProvider({projectId})])
 
 const { webSocketPublicClient } = configureChains(
-  [mainnet],
+  [polygonMumbai],
   [publicProvider()],
 )
 
